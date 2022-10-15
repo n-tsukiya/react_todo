6bca35aedc74f56ffb0c7708d96dce7d1f9569fe
n-tsukiya <tkmaru555@gmail.com>
2022-10-15 21:50:39 +0900

    セクション7-53

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./style.css";
 
 export const App = () => {
   return (
     <>
-      <div className="">
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div className="">
-        <p className="">未完了のTODO</p>
-        <ul className="">
-          <div className="">
-            <li className="">ああああ</li>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div className="">
-            <li className="">いいいい</li>
+          <div className="list-row">
+            <li>いいいい</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div className="">
-        <p className="">完了のTODO</p>
-        <ul className="">
-          <div className="">
-            <li className="">うううう</li>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>うううう</li>
             <button>戻す</button>
           </div>
         </ul>
