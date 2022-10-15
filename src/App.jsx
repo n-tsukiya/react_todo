f2bcaa0e45e799ee28e2f1e0bb5e3f523f595d62
n-tsukiya <tkmaru555@gmail.com>
2022-10-15 21:47:32 +0900

    セクション7-52

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,36 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <div className="">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="">
+        <p className="">未完了のTODO</p>
+        <ul className="">
+          <div className="">
+            <li className="">ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="">
+            <li className="">いいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="">
+        <p className="">完了のTODO</p>
+        <ul className="">
+          <div className="">
+            <li className="">うううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
