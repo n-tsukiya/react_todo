c8a27319cd695fc4446fbe97b73d24939c70ae43
n-tsukiya <tkmaru555@gmail.com>
2022-10-16 12:08:59 +0900

    セクション7-60

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTOODは5個までです。消化して下さい
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
